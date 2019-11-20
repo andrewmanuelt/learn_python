374205edfd5160d16de90e2b81d16ec64a347a72
unknown <andrewmanuelt@gmail.com>
2019-11-20 08:16:27 +0800

    Commit function.py

--- function.py
+++ function.py
@@ -1,3 +1,20 @@
+def kwlength_argument(**kwargs):
+    for key, value in kwargs.items():
+        print(key + ' : ' + value)
+
+def length_argument(*args):
+    var = ''
+
+    for data in args:
+        var += data + ' '
+    return var
+
+def default_argument(arg1, itterate = 10):
+    #Open code below to reset itterate
+    #itterate = 7
+    var = (arg1 + ' ') * itterate
+    return var
+
 def nested_functionI(arg1, arg2):
 
     def inner(word):
@@ -29,6 +46,12 @@ def main():
 
     third = nested_functionII(3)
     print(third('Python '))
+
+    print(default_argument('Hello'))
+
+    print(length_argument("Hello", "python", "scripting"))
     
+    kwlength_argument(name='Patrick', age='14', address='Bikini Bottom')
+
 if __name__ == '__main__':
     main()
